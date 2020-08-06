9580573ee50abf826f402dbe5e645a0b6d65e590
Dipendu <dipenduroy007@gmail.com>
2020-08-06 06:35:40 +0530

    Remove B

--- index1.php
+++ index1.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Dipendu B Roy, this is your first auto deploy using jenkins. All Thanks to git-ftp.";
+echo "Dipendu Roy, this is your first auto deploy using jenkins. All Thanks to git-ftp.";
 ?>
 
